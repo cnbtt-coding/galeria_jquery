e7f8dc0d9c1ce81671062a33a378fdd2d69a0269
Caio Nascimento Battista <caio.caiobattista@outlook.com>
2023-08-22 18:15:16 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 $(document).ready(function() {
+
+    /*
+    Eventos de comportamento do header
+    */
+
     $('header button').click(function() {
-        $('form').slideDown();
+        $('form').slideToggle();
     })
 
-    $('#button-cancel').click(function() { 
+    $('#button-cancel').click(function() {
         $('form').slideUp();
-    });
-});
+    })
+
+    /*
+    Eventos de adição de nova foto
+    */
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#url-new-pic').val();
+        const novoItem = $('<li></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-image-link">
+            <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#url-new-pic').val('');
+    })
+})
